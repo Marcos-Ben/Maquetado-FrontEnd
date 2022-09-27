442ef13a50fbacc1f529430bb26a8376006e9c52
Marcos-Ben <marcosbenencia98@gmail.com>
2022-09-27 16:38:49 -0300

    Maquetado html

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Portfolio-FrontEnd
 repositorio para Portfolio Front End
+para los iconos y demás
+https://fontawesome.com/icons
+https://getbootstrap.com/
+https://pixabay.com/es/
---
+++ script.js
@@ -0,0 +1,13 @@
+function cambiar_parrafo(){
+    document.getElementById("button-acercade").style.display="block";
+let texto= document.getElementById("text-acercade").innerText;
+console.log(texto);
+}
+function myFunction2(valor){
+    document.getElementById("text-acerdade").innerText=valor;
+};
+// carga el contenido de un archivo de texto y lo muestra al parrafo //
+function showFile(input){
+    
+}
+ 
